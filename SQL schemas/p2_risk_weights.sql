2c7a6982699a13eac3ea626e776e65475416bb80
Jovica Božić <112812542+JovicaB@users.noreply.github.com>
2023-11-19 09:48:16 +0100

    Create p2_risk_weights.sql

---
+++ SQL schemas/p2_risk_weights.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `p2_risk_weights` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `risk` varchar(45) DEFAULT NULL,
+  `weights` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
